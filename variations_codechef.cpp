5f5b73a26d9bc9d33eee879c0147511844a19506
anuragpratap05 <56753626+anuragpratap05@users.noreply.github.com>
2020-09-17 20:07:40 +0530

    Create variations_codechef.cpp

---
+++ variations_codechef.cpp
@@ -0,0 +1,28 @@
+# searching-sorting_application
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    int count=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]-a[i]>=k)
+            {
+                count+=(n-j);
+                break;
+            }
+        }
+    }
+    cout<<count<<endl;
+}
